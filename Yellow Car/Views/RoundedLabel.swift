38f9101585b56c5b69dad8018a2f251153e56650
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2018-04-08 20:17:41 -0500

    First commit

---
+++ Yellow Car/Views/RoundedLabel.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedLabel.swift
+//  Yellow Car
+//
+//  Created by Chris McDonald on 4/8/18.
+//  Copyright © 2018 Chris McDonald. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedLabel: UILabel {
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
